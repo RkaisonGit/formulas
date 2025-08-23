dc4250fcb18ecb353311d302c23cb929d4fa3951
Ryan Anibrika <144281346+RkaisonGit@users.noreply.github.com>
2025-08-23 13:12:55 +0000

    Add files via upload

---
+++ pressure.py
@@ -0,0 +1,7 @@
+'''
+Pressure
+'''
+F=float(input("Enter Force:" ))
+A=float(input("Enter accelaration:"))
+P=F/A
+print(P)
---
+++ velocity.py
@@ -0,0 +1,7 @@
+'''
+Velocity
+'''
+D=float(input("Enter displacement:" ))
+T=float(input("Enter time:"))
+V=D/T
+print(V)
